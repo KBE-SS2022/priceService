1125524c897805a9caca1779f9bfb849019f36a8
Eduard Schander <66794307+EddeCCC@users.noreply.github.com>
2022-07-27 01:22:52 +0200

    CI-Pipeline (#3)
    * CI-Pipeline
    
    * fix test

--- src/test/java/com/api/priceService/controller/PriceControllerTest.java
+++ src/test/java/com/api/priceService/controller/PriceControllerTest.java
@@ -5,25 +5,28 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import priceService.api.controller.PriceController;
+import priceService.api.service.PriceService;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(PriceController.class)
 @ContextConfiguration(classes ={PriceController.class})
+@WebMvcTest(PriceController.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class PriceControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private PriceService priceService;
+
     @Test
     void testMockMvcCreation(){
         assertNotNull(mockMvc);
     }
-
 }
